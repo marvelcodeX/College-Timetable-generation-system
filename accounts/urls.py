34f5b11f055c0fb5068d0bfe4c0609c89606848c
J Niveditha <nivedithaja@gmail.com>
2026-05-23 21:36:39 +0530

    Add account authentication app

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+]
